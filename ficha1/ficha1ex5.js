64e11c315c4317d37a5e17cc68206d335f80e86a
vitorpereira267 <2048920@student.uma.pt>
2021-03-11 18:51:49 +0000

    dsadsa

--- ficha1/ficha1ex5.js
+++ ficha1/ficha1ex5.js
@@ -1,6 +1,140 @@
-var a = 10;
-var b = 5;
 
-var c = a + b;
+// Ex5
 
-console.log(c);
+
+var nota1 = 16;
+var nota2 = 10;
+var nota3 = 12;
+var notafinal = nota1 * 0.4 + nota2 * 0.4 + nota3 * 0.4;
+
+// console.log("A avaliação final e':" + Math.round(notafinal) + " valores")
+
+
+// Ex6
+
+//  var month = 6;
+
+// if (month == 1) {
+//     console.log("Janeiro");
+// }
+// else if (month == 2) {
+//     console.log("Fevereiro");
+// }
+// else if (month == 3) {
+//     console.log("Março");
+// }
+// else if (month == 4) {
+//     console.log("Abril");
+// }
+
+// ou
+
+// switch (month) {
+//     case 1:
+//         console.log("Janeiro");
+//         break;
+//     case 2:
+//         console.log("Fevereiro");
+//         break;
+//     case 3:
+//         console.log("Março");
+//         break;
+//     case 4:
+//         console.log("Abril");
+//         break;
+//     case 5:
+//         console.log("Maio");
+//         break;
+//     case 6:
+//         console.log("Junho");
+//         break;
+//     case 7:
+//         console.log("Julho");
+//         break;
+//     case 8:
+//         console.log("Agosto");
+//         break;
+//     case 9:
+//         console.log("Setembro");
+//         break;
+//     case 10:
+//         console.log("Outubro");
+//         break;
+//     case 11:
+//         console.log("Novembro");
+//         break;
+//     case 12:
+//         console.log("Dezembro");
+//         break;
+//     default:
+//         console.log("Mes invalido")
+//         break;
+// }
+
+// Ex7
+
+// var operador1 = 2;
+// var operador2 = 2;
+// var operando = "+";
+// var resultado = 0;
+
+
+// if (operando == "+") {
+//     resultado = operador1 + operador2;
+//     console.log("Soma");
+// }
+// else if(operando == "-"){
+//     resultado = operador1 + operador2;
+//     console.log("Subtracao");
+// }
+// else if(operando == "*"){
+//     resultado = operador1 + operador2;
+//     console.log("Multiplicacao");
+// }
+// else if(operando == "/"){
+//     resultado = operador1 + operador2;
+//     console.log("Divisao");
+// }
+// else if(operando == "^"){
+//     resultado = Math.pow(operador1,operador2);
+//     console.log("Expoente");
+// }
+
+// console.log(resultado);
+
+
+// Ex8
+
+// var i = 0;
+
+// while (i <= 20) {
+//     console.log(i);
+//     i + - 5;
+// }
+
+// // ou
+
+// for (let j = 0; j <= 20; j++) {
+//     if (j % 5 == 0)
+//         console.log(j);
+
+// }
+
+// Ex9
+
+// var soma = 0;
+
+// for (let j = 0; j <= 3; j++) {
+//         sum +- j;
+
+// }
+
+// console.log(soma);
+
+// Ex10
+
+var array
+
+
+
+
