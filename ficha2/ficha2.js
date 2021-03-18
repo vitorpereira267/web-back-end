81704fe4755156bf9745db8eb107f8f9dcff9bc1
vitorpereira267 <2048920@student.uma.pt>
2021-03-18 19:00:08 +0000

    dsadsa

--- ficha2/ficha2.js
+++ ficha2/ficha2.js
@@ -137,18 +137,120 @@ console.log(count);*/
 }
 
 calculateWorking(8,35,0,16,0,30);*/
-
 // Ex6 ---------------------------------------------------------------------
-
-
-function drawRectangle(width, height){
-    var line = "";
-    for (let j = 0; i < height; j++) {
+/*function drawRectangle(width, height) {
+    for (let j = 0; j < height; j++) {
+        var line = "";
         for (let i = 0; i < width; i++) {
-        line+-"*";
+            line += "*";
+            count++;
+        }
+        console.log(line);
+    }
+
+}
+
+drawRectangle(5, 3);*/
+// Ex7 ---------------------------------------------------------------------
+
+/*function drawTriangle(width, height){
+    for (let j = 0; j < height; j++) {
+        var line = "";
+        for (let i = 0; i < j; i++) {
+        line+="*";
         }
     console.log(line);
     }
 }
 
-drawRectangle(5,3);
+drawTriangle(10);*/
+
+
+// Ex8 ----------------------------------------------------------------------
+
+/*function drawBox(width, height){
+    for (let j = 0; j < height; j++) {
+        var line = "";
+        for (let i = 0; i < width; i++) {
+            
+            if(j == 0 || j == height - 1 || i == 0 || i == width - 1){
+                line += "*";
+            }
+            else{
+                line += " ";
+            }   
+        }
+        console.log(line);
+    }
+}
+console.log("");
+
+drawBox(10, 10);*/
+
+// Ex9 ------------------------------------------------------------------------
+
+var student1 = {firstName: "Pedro", lastName: "Marques", age: 20, grade: 16.5, getGrade: function(){;
+return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student2 = {firstName: "David", lastName: "Tiago", age: 17, grade: 14.5, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student3 = {firstName: "Sofia", lastName: "Bond", age: 23, grade: 13.5, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student4 = {firstName: "Marco", lastName: "Saint", age: 19, grade: 17.5, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student5 = {firstName: "Andre", lastName: "Raul", age: 21, grade: 15.5, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student6 = {firstName: "Ana", lastName: "Duarte", age: 20, grade: 12.5, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student7 = {firstName: "Joana", lastName: "Matos", age: 24, grade: 20, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student8 = {firstName: "Paulo", lastName: "Figueira", age: 18, grade: 9.5, getGrade: function(){;
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+var student9 = {firstName: "Ivan",lastName: "Santos",age: 28,grade: 1, getGrade: function(){
+    return this.firstName + " " + this.lastName + "com idade:" + this.age + " teve:" +  this.grade
+}};
+
+var studentsList = [];
+
+studentsList.push(student1);
+studentsList.push(student2);
+studentsList.push(student3);
+studentsList.push(student4);
+studentsList.push(student5);
+studentsList.push(student6);
+studentsList.push(student7);
+studentsList.push(student8);
+studentsList.push(student9);
+
+function displayGrades(array){
+    for (let i = 0; i < array.length; i++) {
+        const student = array[i];
+        console.log(student.firstName + " " + student.lastName + "com idade:" + student.age + "teve:" + student.grade);
+        
+    }
+}
+
+displayGrades(studentsList);
+
+function getBestGrade(array) {
+    var max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if(array[i].grade > max.grade){
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+var BestGrade = getBestGrade(studentsList);
+console.log();
+console.log("Melhor aluno:");
+console.log(BestGrade.getGrade());
+
