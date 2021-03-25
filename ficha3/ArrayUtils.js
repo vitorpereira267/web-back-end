6713b5083b49a8b1ddf2a2bfc02b16469d19cda9
vitorpereira267 <2048920@student.uma.pt>
2021-03-25 18:21:04 +0000

    modi

--- ficha3/ArrayUtils.js
+++ ficha3/ArrayUtils.js
@@ -1,39 +1,108 @@
 var arrayUtils = {
-    isEmpty: function(array){
-        if(array != undefined && array != null){
+    isEmpty: function (array) {
+        if (array != undefined && array != null) {
             return array.length == 0;
         }
-        else{
+        else {
             return "Array is undefined";
         }
     },
-    max: function(array){
+    max: function (array) {
         var m = array[0];
         for (let i = 1; i < array.length; i++) {
-            if(array[i] > m){
+            if (array[i] > m) {
                 m = array[i];
             }
         }
         return m;
     },
 
-    min: function(array){
+    min: function (array) {
         var m = array[0];
         for (let i = 1; i < array.length; i++) {
-            if(array[i] < m){
+            if (array[i] < m) {
                 m = array[i];
             }
         }
         return m;
     },
-    average: function(array){
+    average: function (array) {
         var sum = 0;
         for (let i = 1; i < array.length; i++) {
             sum += array[i];
         }
         var avg = sum / array.length;
         return avg;
-    }
+    },
+
+    indexOf: function (array, value) {
+        for (let i = 1; i < array.length; i++) {
+            if (value == array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    subArray: function (array, startIndex, endIndex) {
+        var a = [];
+        for (let i = startIndex; i <= endIndex; i++) {
+            a.push(array[i]);
+        }
+        return a;
+    },
+
+    isSameLenght: function (array, otherArray) {
+        var I = array.length
+        var Io = otherArray.length;
+
+        if (I == Io) {
+            return true;
+        } else {
+            return false;
+        }
+
+        //return array.lenght == otherArray.length;
+    },
+
+    reverse: function (array) {
+        var r = [];
+        for (let i = array.lenght - 1; i >= 0; i--) {
+            r.push(array[i]);
+        }
+        return r;
+    },
+
+    swap: function (array, index1, index2) {
+        //obter um valor que esta num determinado indice no array
+        var val1 = array[index1];
+        var val2 = array[index2];
+
+        array[index1] = val2;
+        array[index2] = val1;
+
+        return array;
+    },
+
+    contains: function (array, value) {
+
+        for (let i = 0; i < array.length; i++) {
+            if (value == array[i]) {
+                return true;
+            }
+        }
+        return false;
+    },
+
+    concat: function (array, otherArray){
+        var concatArray = array;
+        for (let i = 0; i < otherArray.length; i++) {
+            concatArray.push(otherArray[i]);
+        }
+        return concatArray;
+    },
+
+
 
 
 
--- ficha3/app.js
+++ ficha3/app.js
@@ -24,8 +24,9 @@ function performDownload(startedF, updateF, completedF){
     updateF();
     completedF();
 }
+var log = require("./log.js")
 
-const arrayUtils = require("./ArrayUtils");
+
 //performDownload(started, update, completed);
 
 //==================================================================
@@ -33,8 +34,35 @@ const arrayUtils = require("./ArrayUtils");
 // console.log(log.message);
 // console.log(log.functionObj("Hello"));
 
-var log = require("./ArrayUtils.js")
+const arrayUtils = require("./ArrayUtils");
 var array= [12, 4, 6, 88, 9, 0];
+var otherArray= [12, 4, 6, 88, 9, 0];
+
 console.log("O array esta vazio? " + arrayUtils.isEmpty(array));
 console.log("O maximo do array e: " + arrayUtils.max(array));
 console.log("O minimo do array e: " + arrayUtils.min(array));
+console.log("A media do array e: " + arrayUtils.average(array));
+
+var value = 6;
+console.log("O indice do valor " + value + " e: " + arrayUtils.indexOf(array, value));
+
+var subArray = arrayUtils.subArray(array, 0, 3);
+
+console.log("O sub array e: ", subArray);
+
+var sameSize = arrayUtils.isSameLenght(array, otherArray);
+console.log("Os array sao do mesmo tamanho: " + sameSize);
+
+var invertedArray = arrayUtils.reverse(array);
+console.log("O array invertido e: ", invertedArray);
+
+var swappedArray = arrayUtils.swap(array, 0, 4);
+console.log("O array alterado e: ", swappedArray);
+
+console.log("O aarray contem o valor:"+ value + "? " + arrayUtils.contains(array, value));
+
+var concatArray = arrayUtils.concat(array, otherArray);
+console.log("Concatenar Array: ", concatArray);
+
+
+
