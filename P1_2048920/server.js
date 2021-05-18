d10748a43f1aa471e7d475f234e900daf37c05f5
vitorpereira267 <2048920@student.uma.pt>
2021-05-18 18:25:22 +0100

    dsa

--- P1_2048920/server.js
+++ P1_2048920/server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2')
 const { INTEGER } = require('sequelize')
 const Sequelize = require('sequelize')
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
+//const swaggerDocument = require('./swagger.json')
 
 
 // Instanciar o express
@@ -15,23 +15,23 @@ const port = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
- const sequelize = new Sequelize('projetobackend', 'root', '', {
-     dialect: 'mysql'
- });
+const sequelize = new Sequelize('projetobackend', 'root', '', {
+    dialect: 'mysql'
+});
 
-     sequelize.authenticate()
-         .then(() => {
-             console.log("Connection has been established");
-         })
-         .catch(err => {
-             console.error("Unable to connect", err);
-})
+sequelize.authenticate()
+    .then(() => {
+        console.log("Connection has been established");
+    })
+    .catch(err => {
+        console.error("Unable to connect", err);
+    })
 
- const Product = sequelize.define('product', {
+const Product = sequelize.define('product', {
     id: {
         primaryKey: true,
         type: Sequelize.INTEGER,
@@ -76,56 +76,132 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     },
 })
 
-sequelize.sync({ force: false})
+sequelize.sync({ force: false })
     .then(() => {
-       console.log('Database & tables created!');
+        console.log('Database & tables created!');
     }).then(function () {
         return Product.findAll();
     }).then(function (product) {
         console.log(product)
-})
+    })
 
 
-Product.bulkCreate([
-    {firstName: 'Pedro', lastName: 'Jardim', profession: 'IT', age:62},
-    {firstName: 'Manuel', lastName: 'Matos', profession: 'IT', age:12},
-    {firstName: 'Maria', lastName: 'Figueira', profession: 'IT', age:32},
-    {firstName: 'Ana', lastName: 'Duarte', profession: 'IT', age:82},
-    {firstName: 'Luís', lastName: 'Faria', profession: 'IT', age:42},
-]).then(function (){
-    return Product.findAll();
-}).then(function (product) {
+/*Product.bulkCreate([
+    {seller_id: 1,title:'Computador',description:'Computador Gaming',price:1000,url:"www.worten.pt",views:178,images:"pc gaming",comments:"Muito Bom",tags:"pc"},
+    {seller_id: 2,title:'Rato',description:'Rato Wirelss',price:50,url:"www.worten.pt",views:121,images:"rato wireless",comments:"Bom",tags:"rato"},
+    {seller_id: 3,title:'Teclado',description:'Teclado Mecanico',price:104,url:"www.worten.pt",views:134,images:"teclado mecanico",comments:"Excelente",tags:"teclado"},
+]).then(function(){
+    return Product.findAll()
+}).then(function(product){
     console.log(product)
-});
+})*/
 
 //PARTE B
 
-// a)
+// a) feito
 
-app.get('/product/:id', (req, res) => {
+app.get('/product', (req, res) => {
 
-        id = req.query.id;
-
-        Product.findByPk(id).then(product => {
+    if (req.query.id) {
+        Product.findByPk(req.query.id).then(product => {
             res.send(product);
-        })
+        }).catch(err => {
+            res.status(404).send({ err: 'No product found' });
+        });
+    }
+    else {
+        Product.findAll().then(product => {
+            res.send(product);
+        });
+    }
 });
 
 
-// b)
+// b) feito
+
 app.delete('/product/:id', (req, res) => {
 
     Product.destroy({
-        where:{
+        where: {
             id: req.params.id
         }
-    }).catch(err => {
-        res.status(404).send({err: 'No product found'})
+    }).then(product => {
+        res.send({ "Product deleted": product });
     })
-    res.status(204).send();
+
 });
 
 
-// c)
+// c) Quase feito
 
-app.post('/product')
+app.put('/product/:id/images', (req, res) => {
+
+    Product.update({ images: "C:\\Users\\Turma A\\Downloads\\oi.png" }, {
+        where: {
+            id: req.params.id
+        }
+    }).then((affectedRows) => {
+        Product.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(product => {
+            res.json(product)
+        }).catch(err => {
+            res.status(404).send({ err: 'No product found' })
+        })
+
+    }).catch(err => {
+        res.status(404).send({ err: 'No product found' })
+    })
+
+    /*Product.findOne({
+        where:{
+            id: req.params.id
+        }
+    }).then(product => {
+        res.json({"Updated Image path:":product.images})
+    }).catch(err => {
+        res.status(404).send({err: 'No product found'})
+    })*/
+})
+
+
+// d)
+
+
+app.put('/product/comments', (req, res) => {
+    Product.update({ comments: "Produto Excelente" }, {
+        where: {
+            id: req.query.id
+        }
+    }).then(product => {
+        res.send({ "Comment done:": product })
+    }).catch(err => {
+        res.status(404).send({ err: 'No product found' })
+    })
+
+    /*Product.findOne({
+        where: {
+            id: req.query.id
+        }
+    }).then(product => {
+        res.send({ "Images path:": product.comments })
+    }).catch(err => {
+        res.status(404).send({ err: 'No product found' })
+    })*/
+
+})
+
+// e)
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
